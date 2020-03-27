c3c6193b08a81d0404b0be2136be2c2d8555206b
Hitesh karma <hkarma71@gmail.com>
2020-03-27 23:24:23 +0530

    Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'our-app';
+  name = 'Vedika karma';
+  names = [
+    "Pankaj",
+    "Lalit",
+    "Pankaj",
+    "Lalit"
+  ];
+  show = 5;
+  show1 = 5;
+  countValue= 0;
+  currentName:any;
+  
+  count(){
+    this.countValue++;  
+    // this.currentName = this.
+    if(this.countValue < 5){
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { core } from '@angular/compiler';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,8 @@ import { core } from '@angular/compiler';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
